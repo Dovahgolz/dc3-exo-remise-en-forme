03ec64bbac84aabc3ece12fa28e2e1a7e68adbd5
sacha jacquot <jacquotsacha@gmail.com>
2023-10-04 09:41:33 +0200

    exo3

---
+++ exo3/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTaskButton");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+        if (taskText !== "") {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                ${taskText}
+                <span class="delete">Supprimer</span>
+            `;
+            taskList.appendChild(li);
+            taskInput.value = "";
+        }
+    });
+
+    taskList.addEventListener("click", function (e) {
+        if (e.target.classList.contains("delete")) {
+            e.target.parentElement.remove();
+        }
+    });
+});
